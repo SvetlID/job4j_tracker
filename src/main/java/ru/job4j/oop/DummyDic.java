46a14770431791caa7a59a7edc49c874fa317323
SvetlDI <svetlana24.dubrova@gmail.com>
2022-01-10 20:19:17 +0500

    1.1. Создание объекта [#165452 #248614

---
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,16 @@
+package ru.job4j.oop;
+
+public class DummyDic {
+
+    public String engToRus() {
+        String word = "helarious";
+        return word;
+    }
+
+    public static void main(String[] args) {
+        DummyDic happy = new DummyDic();
+        String eng = happy.engToRus();
+        System.out.println("Веселый " + eng);
+
+    }
+}
