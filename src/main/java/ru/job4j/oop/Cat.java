69b269678e410186240aad5227cc5d94d010ca4e
SvetlDI <svetlana24.dubrova@gmail.com>
2022-01-11 11:23:10 +0500

    1.5 Поля объекта [166083 #249057]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,1 +1,30 @@
-package ru.job4j.oop;public class Cat {    private String food;    public void eat(String meat) {        this.food = meat;    }    private String name;    public void giveNick(String nick) {        this.name = nick;    }    public void show() {        System.out.println(" kotik " + this.name + " el " + this.food);    }    public static void main(String[] args) {        Cat gav = new Cat();        gav.giveNick("Gav");        gav.eat("kotleta");        gav.show();        Cat black = new Cat();        black.giveNick("Black");        black.eat("fish");        black.show();    }}
+package ru.job4j.oop;
+
+public class Cat {
+    private String food;
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    private String name;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(" kotik " + this.name + " el " + this.food);
+    }
+
+    public static void main(String[] args) {
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
+    }
+}
