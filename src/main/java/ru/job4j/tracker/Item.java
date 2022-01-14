3c514b5c60b282b3dd26c7441afc03b4cdcac81c
SvetlDI <svetlana24.dubrova@gmail.com>
2022-01-14 21:39:34 +0500

    6. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,10 +1,19 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
+    }
+
+    public static void main(String[] args) {
+        Arrays.stream(args).forEach(System.out::println);
+        System.out.println("hello from static!");
     }
 
     public Item(String name) {
@@ -14,6 +23,10 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public int getId() {
@@ -31,4 +44,5 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
 }
---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,16 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        LocalDateTime currentDate = item.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String currentDateFormat = currentDate.format(formatter);
+        System.out.println(currentDateFormat);
+    }
+
+}
