35ae3f393633d5c8e6755901b21e29598e53517d
SvetlDI <svetlana24.dubrova@gmail.com>
2022-03-01 09:31:34 +0500

    1. Телефонный справочник на базе ArrayList [#41598 #269990]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -4,9 +4,9 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
-import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class PhoneDictionaryTest {
     @Test
@@ -26,6 +26,6 @@ public class PhoneDictionaryTest {
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
         ArrayList<Person> persons = phones.find("Petrov");
-        assertThat(persons, is(empty()));
+        assertTrue(persons.isEmpty());
     }
 }
