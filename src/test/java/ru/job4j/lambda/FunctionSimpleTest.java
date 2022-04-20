2dddd4b39587b3a0fdad10593fea20d562c78381
SvetlDI <svetlana24.dubrova@gmail.com>
2022-04-20 22:56:25 +0500

    5. Подсчет функции в диапазоне.

--- src/test/java/ru/job4j/lambda/FunctionSimpleTest.java
+++ src/test/java/ru/job4j/lambda/FunctionSimpleTest.java
@@ -8,7 +8,6 @@ import java.util.List;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-
 public class FunctionSimpleTest {
     @Test
     public void whenLinearFunctionThenLinearResults() {
