a3161cb75aa6d874bb397dcb92a167fbd81b5cc0
SvetlDI <svetlana24.dubrova@gmail.com>
2022-04-20 22:53:15 +0500

    2.2. Лямбда блок [#249208 #288562]

--- src/main/java/ru/job4j/lambda/FI.java
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -9,14 +9,20 @@ public class FI {
                 new Attachment("image 3", 120),
                 new Attachment("image 2", 23)
         };
-        Comparator<Attachment> comparator = (left, right) -> Integer.compare(left.getSize(), right.getSize());
+        Comparator<Attachment> comparator = (left, right) -> {
+            System.out.println("compare - " + left.getSize() + " : " + right.getSize());
+            return Integer.compare(left.getSize(), right.getSize());
+        };
         Comparator<String> cmpSize = (left, right) -> {
+            System.out.println("compare - " + left.length() + ":" + right.length());
             return Integer.compare(left.length(), right.length());
         };
         Comparator<String> cmpText = (left, right) -> {
+            System.out.println("compareTo - " + left + ":" + right);
             return left.compareTo(right);
         };
         Comparator<String> cmpDescSize = (left, right) -> {
+            System.out.println("compare - " + right.length() + ":" + left.length());
             return Integer.compare(right.length(), left.length());
         };
     }
